94090301f28c0732ba6b37509a3c6c0a210ff92e
bimalkhimdung <bimalkhimdung@gmail.com>
2023-08-21 16:01:27 +0545

    AUG 21

--- .github/workflows/ssh.sh
+++ .github/workflows/ssh.sh
@@ -7,4 +7,4 @@ ssh_key="$PRIVATE_KEY"
 mkdir -p ~/.ssh
 echo"$ssh_key" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-ssh -i ~/.ssh/id_rsa "devops@${remote_host}""echo 'hello form remote'; ls -l "
+ssh -i ~/.ssh/id_rsa "devops@${remote_host}""echo 'hello form remote'"
